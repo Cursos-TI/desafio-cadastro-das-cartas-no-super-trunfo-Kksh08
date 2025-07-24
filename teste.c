5d13e5ac6c52dce1a62d538eadbf9f29d1339dac
Kksh08 <144119063+Kksh08@users.noreply.github.com>
2025-07-24 03:13:11 +0000

    FINALIZADO

--- teste.c
+++ teste.c
@@ -2,28 +2,53 @@
 
 int main(){ 
    //CARTA 1
-    char codigo[9];
+    char codigo1[2];
     int populaçao1;
     float area1;
     float pib1;
     int pontos1;
 
     //CARTA 2
-    char codigo[9];    
+    char codigo2[2];    
     int populaçao2;
     float area2;
     float pib2;
     int pontos2;
 
-    printf("digite o codido da carta ex:1 a 2: ");
-    scanf("%d", populaçao1);
+    //CARTA 1
+   
+    printf("digite o codigo da carta 1(ex:1 a 2): ");
+    scanf("%1s", codigo1);
+
+    printf("A populaçao: ");
+    scanf("%d", &populaçao1);
 
     printf("area em KM²: ");
     scanf("%f", &area1);
 
-    printf("PIB: ");
-    scanf("%f", pib1);
+    printf("Pib em milhoes: ");
+    scanf("%f", &pib1);
 
+    printf("Quantos pontos turisticos: ");
+    scanf("%d", &pontos1);
+     
+    //CARTA 2
+
+    printf("Digite o codigo da carta 2(ex: 1 a 2): ");
+    scanf("%1s", codigo2);
+
+    printf("A populacao: ");
+    scanf("%d", &populaçao2);
+
+    printf("Area em KM²: ");
+    scanf("%f", &area2);
+
+    printf("Pib em milhoes: ");
+    scanf("%f", &pib2);
+
+    printf("Quantos pontos turisticos: ");
+    scanf("%d", &pontos2);
+    
 
     
    
